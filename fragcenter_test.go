1d08b4ab05934ac5613b18ccf6967c42e4cfca4a
Ant <azucaro@salesforce.com>
2018-11-04 08:17:56 -0500

    Test all XML files found in the test_stats_payloads folder.

--- fragcenter_test.go
+++ fragcenter_test.go
@@ -4,31 +4,36 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
-var parseTests = []string{
-	"resources/test_stats_payloads/multi_stream.xml",
-	"resources/test_stats_payloads/single_stream.xml",
-}
-
 // TestParsing ensures that all of the example payloads provided parse into the struct we defined
 func TestParsing(t *testing.T) {
-	for _, fileName := range parseTests {
+	files, err := filepath.Glob("resources/test_stats_payloads/*xml")
+	if err != nil {
+		t.Errorf("No XML files found in resources/test_stats_payloads.")
+		t.FailNow()
+	}
+
+	for _, fileName := range files {
 		f, err := os.Open(fileName)
 		if err != nil {
 			t.Errorf("Couldn't open the file '%s'.", fileName)
+			continue
 		}
 
 		xmlBytes, err := ioutil.ReadAll(f)
 		if err != nil {
 			t.Errorf("Couldn't read the bytes in the file '%s'.", fileName)
+			continue
 		}
 
 		var streams LiveStreams
 		err = xml.Unmarshal(xmlBytes, &streams)
 		if err != nil {
 			t.Errorf("Couldn't unmarshal the XML in file '%s'.", fileName)
+			continue
 		}
 	}
 }
