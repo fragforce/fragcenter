37ed145cfaa01dfb5bd62a6aac1cfbe574acc6a3
Paulson McIntyre <paul@gpmidi.net>
2023-10-04 17:28:36 -0400

    Added plugin runs to start()

--- lib/brain/core/run.go
+++ lib/brain/core/run.go
@@ -1,7 +1,16 @@
 package core
 
+import "github.com/fragforce/fragcenter/lib/brain/plugin"
+
 // Start begins execution
 func (b Brain) Start() error {
-	// FIXME: Implement
+	for uid, cell := range b.cells {
+		go func(uid string, cell plugin.Cell) {
+			l := cell.L(nil).WithField("uid", uid)
+			if err := cell.Run(); err != nil {
+				l.WithError(err).Error("Failed running cell")
+			}
+		}(uid.String(), cell)
+	}
 	return nil
 }
